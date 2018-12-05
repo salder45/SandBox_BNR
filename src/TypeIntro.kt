4871898d0cad98d3a2b5a22a81aa6b0d20cbdf39
Eder Martinez <edermtz.04@gmail.com>
2018-12-04 21:50:47 -0600

    First commit

---
+++ src/TypeIntro.kt
@@ -0,0 +1,20 @@
+const val MAX_EXPERIENCE: Int =  5000
+
+fun main(args: Array<String>) {
+    //Challenge Code
+    var hasSteed = false
+    var goldAmount = 50
+    val pubName = "Unicorn's Horn"
+    var currentPublicanOnDuty: String
+    val drinks = listOf("Mead","Wine","LaCroix")
+    //
+    val playerName = "Estragon"
+    var experiencePoints = 5
+    experiencePoints+=5
+    println(experiencePoints)
+    println(playerName)
+
+    println("You found the magic mirror!")
+    println("This is what you see when you watch in it: "+playerName.reversed())
+
+}
