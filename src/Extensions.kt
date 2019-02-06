12d49e5b031fda80e7ce3b994566125ae317b16a
Eder Martinez <edermtz.04@gmail.com>
2019-02-06 08:05:33 -0600

    Chapter 18 -> Generic extension functions

--- src/Extensions.kt
+++ src/Extensions.kt
@@ -2,9 +2,12 @@
 
 fun String.addEnthusiasm(amount: Int =1) = this + "!".repeat(amount)
 
-fun Any.easyPrint() = println(this)
+fun <T> T.easyPrint(): T{
+    println(this)
+    return this
+}
 
 fun main(args: Array<String>) {
-    "Madrigal has left the building".addEnthusiasm().easyPrint()
+    "Madrigal has left the building".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
 }
