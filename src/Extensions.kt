2213a1808f2e97a8983f12f45e84316e699dcd68
Eder Martinez <edermtz.04@gmail.com>
2019-02-06 21:11:50 -0600

    Chapter 18 -> Extension properties

--- src/Extensions.kt
+++ src/Extensions.kt
@@ -1,4 +1,6 @@
 
+val String.numVowels
+    get() = count { "aeiouy".contains(it) }
 
 fun String.addEnthusiasm(amount: Int =1) = this + "!".repeat(amount)
 
@@ -10,4 +12,5 @@ fun <T> T.easyPrint(): T{
 fun main(args: Array<String>) {
     "Madrigal has left the building".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
+    "How many vowels?".numVowels.easyPrint()
 }
